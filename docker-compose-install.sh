15d0aaa52a77a5b577e1463a8922de05f78667ba
S4-LI-0U-F4-LL <boomse@hotmail.fr>
2020-10-19 08:42:09 +0000

    script docker and docker-compose install

--- docker-compose-install.sh
+++ docker-compose-install.sh
@@ -1,3 +1,14 @@
+sudo apt-get install curl
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+
+sudo sh get-docker.sh
+
+sudo usermod -aG docker enablis
+
+sudo docker run hello-world
+
+
 
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
